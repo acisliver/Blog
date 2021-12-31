f51a583a9acf5bcccef2cb5d6629affe9e1e5061
acisliver <7273322@naver.com>
2021-12-31 16:40:28 +0900

    Vue proxy 설정

--- src/main/java/kr/ac/blog/web/HelloController.java
+++ src/main/java/kr/ac/blog/web/HelloController.java
@@ -1,14 +1,12 @@
 package kr.ac.blog.web;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequiredArgsConstructor
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("/api/hello")
     public String hello() {
         return "hello";
     }
